a340a0dfe790d5e2a779b7e7c82659f32e6f2fae
highjun10170 <highjun10170@gmail.com>
2024-10-21 20:25:25 +0900

    Add announcement

--- data/announcements.ts
+++ data/announcements.ts
@@ -7,6 +7,12 @@ export interface AnnouncementInfo {
 
 const announcements: AnnouncementInfo[] = [
     {
+        title: "25 겨울 인턴모집 Winter Internship",
+        created: "2024-10-29",
+        pinned: true,
+        path: "https://brunch.co.kr/@kaisticlab/29"
+    },
+    {
         title: '석박사&인턴모집: Prospective Students',
         created: '2023-11-13',
         pinned: true,
