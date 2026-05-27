138b33df058680fe22e2e1d2e3405e38457a1daa
KomaSeo <ysseo0910@gmail.com>
2026-05-27 16:18:21 +0900

    Update announcements.ts

--- data/announcements.ts
+++ data/announcements.ts
@@ -7,7 +7,7 @@ export interface AnnouncementInfo {
 
 const announcements: AnnouncementInfo[] = [
     {
-        title: "“레지던트 수준” AI가 정신과 환자 초진 맡는다",
+        title: "'레지던트 수준' AI가 정신과 환자 초진 맡는다",
         created: "2026-05-26",
         pinned: true,
         path: "https://n.news.naver.com/article/016/0002647547"
