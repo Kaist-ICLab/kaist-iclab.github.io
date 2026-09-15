8e30adf148aa0ae2671283972a154bb5a7b3e274
Faraz Farangizadeh <faraz.farangizadeh@snapp.cab>
2026-09-15 16:57:29 +0900

    revert publication formatter changes

--- data/publications.ts
+++ data/publications.ts
@@ -36,14 +36,7 @@ const parseAuthors = (authors: any): string => {
 
 const parseVenue = (entry: any): string => {
     if (entry["type"] === "article-journal") {
-        const venue = entry.note
-            ? `${entry["container-title"]} (${entry.note})`
-            : entry["container-title"];
-        return [
-            venue,
-            entry.volume ? `Volume ${entry.volume}` : undefined,
-            entry.issue ? `Issue ${entry.issue}` : undefined,
-        ].filter(Boolean).join(", ");
+        return `${entry['container-title']} Volume ${entry.volume}, Issue ${entry.issue}`;
     }
     else if (entry["type"] === "paper-conference") {
         return `${entry["container-title"]}` + (entry["collection-title"] ? ` (${entry["collection-title"]})` : "");
